9cb5a17da9697c22c5e04a791094a0657e3e62a5
minkyeongJ <jomk2221@naver.com>
2022-08-05 15:53:19 +0900

    DesignCSS: 로그인 페이지 UI

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,41 @@
+import {
+  Button,
+  Input,
+  InputSection,
+  JoinBtn,
+  Label,
+  LoginSection,
+  PositionCenter,
+  Title,
+  TitleSection,
+} from "./Styles";
+
+export default function Login() {
+  return (
+    <>
+      <LoginSection>
+        <PositionCenter>
+          <TitleSection>
+            <Title>오늘도 알찬 하루!</Title>
+          </TitleSection>
+          <InputSection>
+            <Label htmlFor="input_id">아이디</Label>
+            <Input
+              type="text"
+              id="input_id"
+              placeholder="아이디를 입력하세요."
+            />
+            <Label htmlFor="input_pw">비밀번호</Label>
+            <Input
+              type="password"
+              id="input_pw"
+              placeholder="비밀번호를 입력하세요."
+            />
+          </InputSection>
+          <Button>login</Button>
+          <JoinBtn>회원가입</JoinBtn>
+        </PositionCenter>
+      </LoginSection>
+    </>
+  );
+}
